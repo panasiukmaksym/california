ca3afd4f19a68fea043bc5e5ba165671c65d7ca7
Maksym Panasiuk <mak7devel@gmail.com>
2022-09-13 02:31:23 +0300

    added phone menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,32 @@
 document.querySelector('.search').addEventListener('click', (event) => {
     if (event.path[1].classList[0] == 'search__card' || event.path[0].classList[0] == 'search__card') {
-        document.querySelector('.search__input').value = event.target.innerText;
+        document.querySelector('.search__input').value = event.target.textContent;
     }
+    // console.log(event.target.textContent);
+    // console.log(event.target);
+});
+
+const overflow = document.querySelector('.overflow');
+const menu = document.querySelector('.menu');
+overflow.addEventListener('click', (event) => {
+    if ( event.target === overflow || event.target === document.querySelector('.menu__close img') ) {
+        setTimeout( function(){
+            overflow.style.display = 'none';
+        }, 500);
+        menu.style.marginLeft = '-100%';
+        setTimeout( function(){
+            overflow.style.opacity = 0;
+        }, 300);
+    }
+});
+
+const menuButton = document.querySelector('.nav__menu');
+menuButton.addEventListener('click', (event)=> {
+    setTimeout( function(){
+        menu.style.marginLeft = '0%';
+    }, 200);
+    overflow.style.display = 'block';
+    setTimeout( function(){
+        overflow.style.opacity = 1;
+    }, 100);
 });
